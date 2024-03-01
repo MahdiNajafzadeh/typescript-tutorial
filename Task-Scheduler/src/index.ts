71efcd9808ddbc2a554836ac3b245a88215f356c
Mahdi <86781212+MahdiNajafzadeh@users.noreply.github.com>
2024-03-01 05:37:22 +0330

    [Fix] Build Bugs

--- Task-Scheduler/src/index.ts
+++ Task-Scheduler/src/index.ts
@@ -1,9 +1,13 @@
 import express = require("express")
 import dotenv = require("dotenv")
+import bodyParser = require("body-parser")
 import router from "./routes"
+
 dotenv.config()
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(router)
 app.listen(process.env.PORT, () => {
 	console.log(`Server Start : PORT : ${process.env.PORT}`)
